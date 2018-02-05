a7fc6388a5037708d359b4b130603b2962f19278
houyaoming <yaoming.hou@i-tudou.com>
2018-02-05 15:19:36 +0800

    工作量证明: 与前一区块的proof拼接成的字符串hash值以4个0开头

--- BlockChains/BlockChain.py
+++ BlockChains/BlockChain.py
@@ -3,6 +3,7 @@
 import hashlib
 import json
 from time import time
+from uuid import uuid4
 
 
 class Blockchain(object):
@@ -14,12 +15,12 @@ class Blockchain(object):
         self.new_block(previous_hash=1, proof=100)
 
     def new_block(self, proof, previous_hash=None):
-        ''''''
+        """
         Creates a new Block and adds it to the chain
         :param proof: <int> The proof given by the Proof of Work algorithm
         :param previous_hash: (Optional) <str> Hash of previous Block
         :return: <dict> New Block
-        ''''''
+        """
         block = {
             'index': len(self.chain) + 1,
             'timestamp': time(),
@@ -35,13 +36,13 @@ class Blockchain(object):
         return block
 
     def new_transaction(self, sender, recipient, amount):
-        ''''''
+        """
         Generate new transaction information, the information will be added to the next block to be dug
         :param sender: <str> Address of the Sender
         :param recipient: <str> Address of the Recipient
         :param amount: <int> Amount
         :return: <int> The index of the block that will hold this transaction
-        ''''''
+        """
 
         self.current_transactions.append({
             'sender': sender,
@@ -54,11 +55,11 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-        ''''''
+        """
         Hashes a Block with SHA-256 algorithm
         :param block: <dict> Block
         :return: <str>
-        ''''''
+        """
         # We must make sure that the Dictionary is Ordered, or we'll have inconsistent hashes
         block_string = json.dumps(block, sort_keys=True).encode()
         return hashlib.sha256(block_string).hexdigest()
@@ -67,3 +68,34 @@ class Blockchain(object):
     def last_block(self):
         # Returns the last Block in the chain
         return self.chain[-1]
+
+    def proof_of_work(self, last_proof):
+        """
+        简单的工作量证明：
+        － 查找一个p'使得hash(pp')以４个０开头
+        － p是上一个块的证明，p'是当前的证明
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        验证证明: 是否hash(last_proof, proof)以４个０开头
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not
+        """
+
+        guess = ("%d%d" % (last_proof, proof)).encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
+    
+
