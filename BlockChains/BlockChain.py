2ca5af810274d500c17e03a19f5888c11f205371
houyaoming <yaoming.hou@i-tudou.com>
2018-02-05 11:41:46 +0800

    加入交易，返回记录被添加到的区块(下一个待挖掘的区块)

--- BlockChains/BlockChain.py
+++ BlockChains/BlockChain.py
@@ -10,9 +10,23 @@ class Blockchain(object):
         # Creates a new Block and adds it to the chain
         pass
 
-    def new_transaction(self):
-        # Adds a new transaction to the list of transactions
-        pass
+    def new_transaction(self, sender, recipient, amount):
+        ''''''
+        Generate new transaction information, the information will be added to the next block to be dug
+        :param sender: <str> Address of the Sender
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the block that will hold this transaction
+        ''''''
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
+
 
     @staticmethod
     def hash(self):
